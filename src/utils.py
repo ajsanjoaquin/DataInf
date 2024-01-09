ee482c794126e10d35f2853e8e7f91296410c384
AJ San Joaquin <ajsanjoaquin@gmail.com>
2024-01-09 16:09:11 +0800

    fix input for prompt format

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 def generate_prompt(instruction, input=None):
-    if input or input is not np.nan:
+    if input is not np.nan or input:
         return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
 ### Instruction:
